703948ae7baee67a553525623420cd6ba0271b08
Mooseman <vikasmk@gmail.com>
2023-03-26 00:57:13 +0530

    Implemented wrap around for the ship

--- godot_file/scenes/ship.gd
+++ godot_file/scenes/ship.gd
@@ -5,7 +5,10 @@ var SPEED = 0
 var screen_size # Size of the game window.
 var direction
 @export var rotation_speed_rad = 0.1
+
+@export var velocity_damping_factor = 0.05 
 var rotation_update
+var myvelocity = Vector2.ZERO
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -15,15 +18,15 @@ func _ready():
 
 
 func _physics_process(delta):
-	var myvelocity = Vector2.ZERO
-	rotation_update =  0.0 
-	
+	myvelocity = (1-velocity_damping_factor) * myvelocity
+	rotation_update =  0.0
+
 	if Input.is_action_pressed("ui_forward"):
 		print("FORWARD")
 		myvelocity.x += SPEED * cos(direction.angle())
 		myvelocity.y += SPEED * sin(direction.angle())
-		print("Velocity",velocity)
-		print("Angle",myvelocity.angle())
+		print("Velocity",myvelocity)
+		print("Angle",(PI/180.0)*myvelocity.angle())
 	if Input.is_action_pressed("ui_rot_clock_wise"):
 		print("ROTATE CLOCKWISE")
 		direction = direction.rotated(rotation_speed_rad)
@@ -32,15 +35,14 @@ func _physics_process(delta):
 		print("ROTATE Counter clockwise")
 		direction = direction.rotated(-rotation_speed_rad)
 		rotation_update = -rotation_speed_rad
-	rotation += rotation_update 
-	position += myvelocity * delta 
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	rotation = (rotation + rotation_update)
+	position += myvelocity * delta
+	if(position.x > screen_size.x ):
+		position.x = 0
+	if(position.x < 0 ):
+		position.x = screen_size.x
+	if(position.y > screen_size.y ):
+		position.y = 0
+	if(position.y < 0):
+		position.y = screen_size.y
 	
-	
-	
-		
-	
-	
-
-	move_and_slide()
